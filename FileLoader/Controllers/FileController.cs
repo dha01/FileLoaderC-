56669f49c6740ceca64b8bc683ed137e98b01285
dha <dha01@yandex.ru>
2016-01-28 15:10:04 +0500

    Initial

---
+++ FileLoader/Controllers/FileController.cs
@@ -0,0 +1,101 @@
+﻿using System.Diagnostics;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using FileLoader.Models.Service.File;
+
+namespace FileLoader.Controllers
+{
+	public class FileController : Controller
+	{
+		private FileService _fielService;
+
+		private string _path = "F://Files//";
+
+		public FileController()
+		{
+			_fielService = new FileService();
+		}
+
+		[AllowAnonymous]
+		public ActionResult Index()
+		{
+			return View(_fielService.GetFileList(_path));
+		}
+
+		/// <summary>
+		/// Возвращает файл с сервера.
+		/// </summary>
+		/// <param name="FileName">Имя файла.</param>
+		/// <returns></returns>
+		[AllowAnonymous]
+		public FileResult Download(string FileName)
+		{
+			var result = File(_path + FileName, System.Net.Mime.MediaTypeNames.Application.Octet, FileName);
+			return result;
+		}
+
+		/// <summary>
+		/// Загружает файл на сервер..
+		/// </summary>
+		/// <param name="file">Файл.</param>
+		/// <returns></returns>
+		[HttpPost]
+		public ActionResult Upload(HttpPostedFileBase file)
+		{
+			var new_name = _fielService.SaveFile(file, _path);
+			return Json(new { new_name }, JsonRequestBehavior.AllowGet);
+		}
+
+		/// <summary>
+		/// Загрузка файла, выполнение подпрограммы и возврат резульитата.
+		/// </summary>
+		/// <param name="file">Файл.</param>
+		/// <param name="operation_name">Название файла подпрограммы.</param>
+		/// <returns>Файл с результатом.</returns>
+		[HttpPost]
+		public FileResult UploadAndRunOperation(HttpPostedFileBase file, string operation_name)
+		{
+			// Получаем имя файла.
+			var file_name = Path.GetFileName(file.FileName);
+
+			// Сохраняем файл на диск.
+			var new_name = _fielService.SaveFile(file, _path);
+
+			// Имя файла с результатом.
+			var result_file_name = string.Format("OutFile_{0}", new_name);
+			
+			// Создаем новый процесс.
+			Process proc = new Process
+			{
+				StartInfo =
+				{
+					// Имя запускаемой подпрограммы.
+					FileName = string.Format(@"{0}\{1}", _path, operation_name),
+					// Аргументы для запуска.
+					Arguments = string.Format(@"{0}{1} {0}{2}", _path, new_name, result_file_name)
+				}
+			};
+
+			// Запускаем процесс.
+			proc.Start();
+
+			// Ожидаем завершения.
+			proc.WaitForExit();
+
+			// Возвращаем файл.
+			return File(_path + result_file_name, System.Net.Mime.MediaTypeNames.Application.Octet, string.Format(@"Result_{0}_{1}",new_name, file_name));
+		}
+
+		/// <summary>
+		/// Удаляет файл
+		/// </summary>
+		/// <param name="FileName">Имя файла.</param>
+		/// <returns></returns>
+		public ActionResult Remove(string FileName)
+		{
+			System.IO.File.Delete(_path + FileName);
+			return Redirect(HttpContext.Request.UrlReferrer.AbsoluteUri);
+		}
+	}
+}
---
+++ FileLoader/Models/Service/File/FileService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace FileLoader.Models.Service.File
+{
+	public class FileService
+	{
+
+		public List<string> GetFileList(string location)
+		{
+			var dir = new DirectoryInfo("F://Files//");
+			FileInfo[] file_names = dir.GetFiles("*.*");
+			List<string> list = new List<string>();
+
+			foreach (var file in file_names)
+			{
+				list.Add(file.Name);
+			}
+			return list;
+		}
+
+		public string SaveFile(HttpPostedFileBase file, string location)
+		{
+			if (file.ContentLength == 0)
+			{
+				throw new Exception("Данные отсутствуют.");
+			}
+
+			var new_name = Guid.NewGuid().ToString();
+			var path = Path.Combine(location, new_name);
+			file.SaveAs(path);
+			return new_name;
+		}
+	}
+}
